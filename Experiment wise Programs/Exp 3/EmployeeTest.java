d2bbbeaa383f7197311aba5be9b7dcb52fd6a217
Shubham Pattewar <163441869+shubham-pattewar@users.noreply.github.com>
2025-03-17 10:48:14 +0530

    Add files via upload

---
+++ Experiment wise Programs/Exp 3/EmployeeTest.java
@@ -0,0 +1,22 @@
+class Employee{
+    String name, address;
+    double salary;
+}
+
+class Manager extends Employee{
+
+}
+
+class Developer extends Employee{
+
+}
+
+class Programmer extends Employee{
+
+}
+
+class EmployeeTest{
+    public static void main(String[] args) {
+        
+    }
+}
